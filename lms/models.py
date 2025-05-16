77739a0b8556e9777f077c6b7310bcd6835d1653
Komp <artemshoshura@mail.ru>
2025-05-16 22:06:53 +0400

    реализованы два приложения users и ims, созданы модели user, course, lesson, реализованы CRUD и generic классы, сериализаторы

---
+++ lms/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    preview = models.ImageField(upload_to='course_previews/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    preview = models.ImageField(upload_to='lesson_previews/')
+    video_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.course.name} - {self.name}"
+
